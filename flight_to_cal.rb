04bc7dcf41cebda22e2818b56dd8fa111257e0f8
Alex Levine <a17levine@gmail.com>
2015-06-06 01:18:32 -0500

    things work

--- flight_to_cal.rb
+++ flight_to_cal.rb
@@ -1,7 +1,4 @@
 require 'sinatra'
-# require 'sinatra/base'
-# require 'sinatra/json'
-require 'json'
 require 'dotenv'
 require 'bundler/setup'
 require 'flightstats'
@@ -17,27 +14,14 @@ set :server, 'thin'
 # Set public folder location
 set :public_folder, File.dirname(__FILE__) + '/public'
 
-
-get '/' do
-	erb :index
-end
-
-post '/get_flight' do
-	content_type :json
-	airline_code = params['airlineCode']
-	flight_number = params['flightNumber']
-	departure_date = params['departureDate']
-	{airlineCode: airline_code, flightNumber: flight_number, departureDate: departure_date}.to_json
-end
-
-@all_airports = JSON.parse(File.read('airports.json'))
+@@all_airports = JSON.parse(File.read('airports.json'))
 
 FlightStats.app_id = ENV['FLIGHTSTATS_APP_ID']
 FlightStats.app_key = ENV['FLIGHTSTATS_APP_KEY']
 
 def airport_code_to_city_name(airport_code)
 	begin
-		@all_airports.find { |a| a['code'] == airport_code.to_s.upcase }['city']
+		@@all_airports.find { |a| a['code'] == airport_code.to_s.upcase }['city']
 	rescue
 		'City not found'
 	end
@@ -45,10 +29,10 @@ end
 
 def airport_code_to_address(airport_code)
 	begin
-		airport = @all_airports.find { |a| a['code'] == airport_code.to_s.upcase }
+		airport = @@all_airports.find { |a| a['code'] == airport_code.to_s.upcase }
 		return "#{airport['name']}, #{airport['city']}, #{airport['state']}, #{airport['country']}"
 	rescue
-		'Address not found'
+		''
 	end
 end
 
@@ -61,12 +45,31 @@ def flight_to_gcal(carrier_code, flight_number, departure_year, departure_month,
 		long_description = URI.escape("For status updates: https://www.google.com?#q=#{URI.escape(carrier_code.to_s + ' ' + flight_number.to_s)}")
 		url = "https://www.google.com/calendar/render?action=TEMPLATE&text=#{URI.escape(name_of_event)}&dates=#{departure_time_for_url}/#{arrival_time_for_url}&details=#{long_description}&location=#{URI.escape(airport_code_to_address(flight.departure_airport_fs_code))}&trp=false"
 		return url
-	rescue
-		return 'Flight not found'
 	end
 end
 
+get '/' do
+	erb :index
+end
 
+post '/get_flight' do
+	content_type :json
+	airline_code = params['airlineCode'].to_s
+	flight_number = params['flightNumber'].to_i
+	departure_date = params['departureDate'].to_s
+	departure_year = departure_date.split('-')[0].to_i
+	departure_month = departure_date.split('-')[1].to_i
+	departure_day = departure_date.split('-')[2].to_i
+	
+
+	# flight_to_gcal(airline_code, flight_number, departure_year, departure_month, departure_day)
+	# foobar = flight_to_gcal('AA', 456, 2015, 6, 7)
+	{
+		gcalUrl: flight_to_gcal(airline_code, flight_number, departure_year, departure_month, departure_day), 
+		description: 'hi',
+		error: 'whoops'
+	}.to_json
+end
 
 # puts flight_to_gcal('KL', 662, 2015, 6, 3)
 
